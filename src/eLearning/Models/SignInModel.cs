9301b2cc03daaf11227dde135dc0c6db7a572ded
zrajnis <zvone.rajnis@gmail.com>
2017-03-31 18:51:20 +0200

    Sign in logic, angular router

---
+++ src/eLearning/Models/SignInModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eLearning.Models
+{
+    [NotMapped]
+    public class SignInModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+        [Required, RegularExpression(@"^(?=.*\d).{6,}$")]
+        public string Password { get; set; }
+    }
+}
