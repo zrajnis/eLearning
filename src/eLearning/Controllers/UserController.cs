587098cb4e9e3783730cdaaa1b76d9f7608ff857
zrajnis <zvone.rajnis@gmail.com>
2017-04-03 15:55:39 +0200

    Overrode return url for failed authorization.

--- src/eLearning/Controllers/UserController.cs
+++ src/eLearning/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,6 +12,7 @@ namespace eLearning.Controllers
     public class UserController : Controller
     {
         // GET: /<controller>/
+        [Authorize]
         public IActionResult Index()
         {
             return View();
